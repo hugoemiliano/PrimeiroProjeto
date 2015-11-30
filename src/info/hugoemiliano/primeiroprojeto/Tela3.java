f19bfc0ec8e6096be20aaa775985c7581c078b19
Hugo Emiliano <heavila@gmail.com>
2015-11-30 19:50:48 -0200

    Relative Layout

---
+++ src/info/hugoemiliano/primeiroprojeto/Tela3.java
@@ -0,0 +1,17 @@
+package info.hugoemiliano.primeiroprojeto;
+
+import java.util.Set;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tela3 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.relative_activity_tela3);
+	}
+
+}
